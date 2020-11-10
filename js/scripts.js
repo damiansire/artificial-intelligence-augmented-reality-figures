1566bdfc143ddf4f12d02533babae8b045b26ecd
damiansire <damiansire@hotmail.com>
2020-11-10 01:29:09 -0300

    add sliders for changes

--- js/scripts.js
+++ js/scripts.js
@@ -9,33 +9,11 @@ let figureName;
 
 let figureSize = 200;
 
-document.querySelector("#figureSize").addEventListener("input", (event) => {
-    figureSize = int(event.data);
-})
-
-function drawFigureFunction(figureName) {
-    switch (figureName) {
-        case "Cuadrado":
-            plane(70);
-            break;
-        case "Cubo":
-            box(70, 70, 70);
-            break;
-        case "Cilindro":
-            cylinder(70, 70);
-            break;
-        case "Cono":
-            cone(70, 70);
-            break;
-        case "Toro":
-            torus(70, 20);
-            break;
-        case "Esfera":
-            sphere(70);
-            break;
-    }
-}
-
+//Slider
+let backgroundSliders;
+let figureColorSliders;
+let figureSizeSlider;
+let figureRotateSlider;
 
 //Setup, es de processing, se inicia una sola vez
 function setup() {
@@ -63,6 +41,18 @@ function setup() {
         button.addEventListener("click", (event) => {
             figureName = event.target.getAttribute("data-id");
         }))
+
+    //Los sliders
+    let acumulateHeight = 10;
+    backgroundSliders = createSlidersInProcessing(width, acumulateHeight, 3, "Colores de fondo")
+    acumulateHeight += getSlidersWithTextHeight(3);
+    figureColorSliders = createSlidersInProcessing(width, acumulateHeight, 3, "Color de la figura")
+    acumulateHeight += getSlidersWithTextHeight(3);
+    figureSizeSlider = createSliderInProcessing(width, acumulateHeight, "Tamaño", 0, 100, 100);
+    acumulateHeight += getSlidersWithTextHeight(1);
+    figureRotateSlider = createSliderInProcessing(width, acumulateHeight, "Angular de rotacion", 0, 100, 100);
+    acumulateHeight += getSlidersWithTextHeight(1);
+
 }
 
 
@@ -76,6 +66,7 @@ function draw() {
     if (lastPrediction != null) {
         drawFigure(lastPrediction[0], lastPrediction[1], figureSize)
     }
+
 }
 
 // Dibuja los puntos como elipse/circulo
@@ -99,11 +90,118 @@ function getLastPrediction() {
 }
 
 function drawFigure(x, y, z) {
+    //applyBackgroundColor se activa realmente unicamente si quiere color
+    applyBackgroundColor()
     translate(x, y, z);
     push();
     rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
+    applyBackgroundColorToFigure()
     drawFigureFunction(figureName)
     pop();
+}
+
+document.querySelector("#figureSize").addEventListener("input", (event) => {
+    figureSize = int(event.data);
+})
+
+//Un strategy pattern medio turbio xD
+function drawFigureFunction(figureName) {
+    switch (figureName) {
+        case "None":
+            break;
+        case "Cuadrado":
+            return plane(70);
+            break;
+        case "Cubo":
+            return box(70, 70, 70);
+            break;
+        case "Cilindro":
+            return cylinder(70, 70);
+            break;
+        case "Cono":
+            return cone(70, 70);
+            break;
+        case "Toro":
+            return torus(70, 20);
+            break;
+        case "Esfera":
+            return sphere(70);
+            break;
+    }
+}
+
+optionsButton = ["colorButton", "backgroundButton"]
+
+buttonState = optionsButton.map(buttonClass => { buttonClass: false });
+
+//Agrego los detectores de click a los botoens
+optionsButton.forEach(buttonClass => {
+    let button = document.getElementsByClassName(buttonClass)[0];
+    button.addEventListener("click", () => {
+        buttonState[buttonClass] = !buttonState[buttonClass];
+    })
+})
+
+function applyBackgroundColor() {
+    if (buttonState["backgroundButton"]) {
+        const r = backgroundSliders[0].value();
+        const g = backgroundSliders[1].value();
+        const b = backgroundSliders[2].value();
+        background(r, g, b);
+    }
+}
+
+function applyBackgroundColorToFigure() {
+    if (buttonState["colorButton"]) {
+        const r = figureColorSliders[0].value();
+        const g = figureColorSliders[1].value();
+        const b = figureColorSliders[2].value();
+        fill(r, g, b);
+    }
+}
+
+
+// Hay un bug de processing a la hora de colocar los textos con videos
+// Por lo cual creo la funcion yo
+// ATT EL DAMI
+function setTextInProcessing(text, positionX, positionY) {
+    let spanElement = document.createElement("span");
+    //Si vas a cambiar algo, no cambies esto
+    //Uso textNode para no regenerar el arbol
+    //NO CAMBIAR POR INNER HTML o alguna variante
+    let textElement = document.createTextNode(text);
+    //Le agrego el texto al elemento span
+    spanElement.appendChild(textElement);
+    //Te devuelve una referencia al elemento html
+    let spanRef = document.getElementsByTagName("body")[0].appendChild(spanElement)
+    spanRef.style.position = "absolute";
+    spanRef.style.left = positionX + "px";
+    spanRef.style.top = positionY + "px";
+    return spanRef;
+}
+
+function createSlidersInProcessing(positionX, positionY, sliderAmount, sliderText) {
+    slidersRef = []
+    setTextInProcessing(sliderText, positionX, positionY)
+    for (let sliderIndex = 0; sliderIndex < sliderAmount; sliderIndex++) {
+        let rSlider = createSlider(0, 255, 100);
+        rSlider.position(positionX, positionY + 20 + 30 * sliderIndex);
+        slidersRef.push(rSlider)
+    }
+    return slidersRef;
+}
+
+function createSliderInProcessing(positionX, positionY, sliderText, fromValue, toValue, realValue) {
+    setTextInProcessing(sliderText, positionX, positionY)
+    let slidersRef = createSlider(fromValue, toValue, realValue);
+    slidersRef.position(positionX, positionY + 20);
+    return slidersRef;
+}
+
+
+//Obtener cuanto ocupan x cantidad de sliders con texto (en px)
+function getSlidersWithTextHeight(slidersAmount) {
+    return 20 + 30 * slidersAmount;
 }
