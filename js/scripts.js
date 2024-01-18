191a984970bcc25887daf7f791296b1de71aa1eb
Damian Sire <damiansire@hotmail.com>
2024-01-17 23:32:03 -0300

    chore: add Prettier and apply formatting corrections

--- js/scripts.js
+++ js/scripts.js
@@ -15,149 +15,143 @@ let figureColorSliders;
 let figureSizeSlider;
 let figureRotateSlider;
 
-//Setup, es de processing, se inicia una sola vez
 function setup() {
-
-    const canvasContainer = document.getElementsByClassName('canvas-container')[0]
-    //Creo el lienzo/rectangulo donde voy a dibujar
-    let canvas = createCanvas(640, 400, WEBGL);
-    canvas.position(0, 0);
-    canvas.style('position', 'absolute');
-    canvas.style('width', '100%');
-    canvas.style('height', '100%');
-    canvas.parent(canvasContainer);
-    /*
- let contenedorAncho = contenedor.offsetWidth;
+  const canvasContainer = document.getElementsByClassName("canvas-container")[0];
+  //Creo el lienzo/rectangulo donde voy a dibujar
+  let canvas = createCanvas(640, 400, WEBGL);
+  canvas.position(0, 0);
+  canvas.style("position", "absolute");
+  canvas.style("width", "100%");
+  canvas.style("height", "100%");
+  canvas.parent(canvasContainer);
+  /*
+    let contenedorAncho = contenedor.offsetWidth;
     let contenedorAlto = contenedor.offsetHeight;
-    */
+  */
 
+  //Capturo en tiempo real mi webcam
+  video = createCapture(VIDEO);
+  //Elijo el tamaño de mi video/webcam a mostrar
+  video.size(width, height);
 
+  function modelReady() {
+    console.log("hand pose loaded");
+    hideModelLoader();
+    handpose.on("predict", (results) => {
+      predictions = results;
+    });
+  }
 
-    //Capturo en tiempo real mi webcam
-    video = createCapture(VIDEO);
-    //Elijo el tamaño de mi video/webcam a mostrar
-    video.size(width, height);
+  //Le aplico a mi video, el modelo de inteligencia artificial que detecta mi mano
+  handpose = ml5.handpose(video, modelReady);
 
-    function modelReady() {
-        console.log('hand pose loaded');
-        hideModelLoader()
-        handpose.on("predict", results => {
-            predictions = results;
-        });
-      }
+  // Escucho a mi video y modelo, y cuando hay una nueva prediccion
+  // Actualizo los puntos del array (Mi mano)
 
-    //Le aplico a mi video, el modelo de inteligencia artificial que detecta mi mano
-    handpose = ml5.handpose(video,modelReady);
+  // Oculto el video
+  video.hide();
 
-    // Escucho a mi video y modelo, y cuando hay una nueva prediccion
-    // Actualizo los puntos del array (Mi mano)
+  figureName = "Cubo";
 
+  document.querySelectorAll(".figureButton").forEach((button) =>
+    button.addEventListener("click", (event) => {
+      figureName = event.target.getAttribute("data-id");
+    })
+  );
 
-    // Oculto el video
-    video.hide();
-
-    figureName = "Cubo";
-
-    document.querySelectorAll(".figureButton").forEach(button =>
-        button.addEventListener("click", (event) => {
-            figureName = event.target.getAttribute("data-id");
-        }))
-
-        /*
-    //Los sliders
-    let acumulateHeight = 100;
-    backgroundSliders = createSlidersInProcessing(width + 30, acumulateHeight, 3, "Colores de fondo")
-    acumulateHeight += getSlidersWithTextHeight(3);
-    figureColorSliders = createSlidersInProcessing(width + 30, acumulateHeight, 3, "Color de la figura")
-    acumulateHeight += getSlidersWithTextHeight(3);
-    figureSizeSlider = createSliderInProcessing(width + 30, acumulateHeight, "Tamaño", 0, 100, 100);
-    figureSizeSlider.value(100 / 2)
-    acumulateHeight += getSlidersWithTextHeight(1);
-    figureRotateSlider = createSliderInProcessing(width + 30, acumulateHeight, "Angular de rotacion", 0, 10, 1);
-    acumulateHeight += getSlidersWithTextHeight(1);
-    */
-
+  /*
+//Los sliders
+let acumulateHeight = 100;
+backgroundSliders = createSlidersInProcessing(width + 30, acumulateHeight, 3, "Colores de fondo")
+acumulateHeight += getSlidersWithTextHeight(3);
+figureColorSliders = createSlidersInProcessing(width + 30, acumulateHeight, 3, "Color de la figura")
+acumulateHeight += getSlidersWithTextHeight(3);
+figureSizeSlider = createSliderInProcessing(width + 30, acumulateHeight, "Tamaño", 0, 100, 100);
+figureSizeSlider.value(100 / 2)
+acumulateHeight += getSlidersWithTextHeight(1);
+figureRotateSlider = createSliderInProcessing(width + 30, acumulateHeight, "Angular de rotacion", 0, 10, 1);
+acumulateHeight += getSlidersWithTextHeight(1);
+*/
 }
 
-
 function draw() {
-    //Toma mi video y lo dibuja muchas veces
-    image(video, -width / 2, -height / 2, width, height);
-    translate(-width / 2, -height / 2)
-        // Dibuja los puntos resultantes de mi mano
+  //Toma mi video y lo dibuja muchas veces
+  image(video, -width / 2, -height / 2, width, height);
+  translate(-width / 2, -height / 2);
+  // Dibuja los puntos resultantes de mi mano
 
-    let lastPrediction = getLastPrediction();
-    if (lastPrediction != null) {
-        drawFigure(lastPrediction[0], lastPrediction[1], figureSize)
-    }
-
+  let lastPrediction = getLastPrediction();
+  if (lastPrediction != null) {
+    drawFigure(lastPrediction[0], lastPrediction[1], figureSize);
+  }
 }
 
 // Dibuja los puntos como elipse/circulo
 function drawKeypoints() {
-    for (i = 0; i < predictions.length; i += 1) {
-        const prediction = predictions[i];
-        for (let j = 0; j < prediction.landmarks.length; j += 1) {
-            const keypoint = prediction.landmarks[j];
-            fill(0, 255, 0);
-            noStroke();
-            ellipse(keypoint[0], keypoint[1], 10, 10);
-        }
+  for (i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
     }
+  }
 }
 
 function getLastPrediction() {
-    if (predictions[0] && predictions[0].landmarks) {
-        return predictions[0].landmarks[2];
-    }
-    return null;
+  if (predictions[0] && predictions[0].landmarks) {
+    return predictions[0].landmarks[2];
+  }
+  return null;
 }
 
 function drawFigure(x, y, z) {
-    let angleRotation = figureRotateSlider?.value() / 10 || 0.1;
-    //applyBackgroundColor se activa realmente unicamente si quiere color
-    applyBackgroundColor()
-    translate(x, y, z);
-    push();
-    rotateZ(frameCount * angleRotation);
-    rotateX(frameCount * angleRotation);
-    rotateY(frameCount * angleRotation);
-    applyBackgroundColorToFigure()
-    drawFigureFunction(figureName)
-    pop();
+  let angleRotation = figureRotateSlider?.value() / 10 || 0.1;
+  //applyBackgroundColor se activa realmente unicamente si quiere color
+  applyBackgroundColor();
+  translate(x, y, z);
+  push();
+  rotateZ(frameCount * angleRotation);
+  rotateX(frameCount * angleRotation);
+  rotateY(frameCount * angleRotation);
+  applyBackgroundColorToFigure();
+  drawFigureFunction(figureName);
+  pop();
 }
-
 
 //Un strategy pattern medio turbio xD
 function drawFigureFunction(figureName) {
-    let sliderSizeValue = figureSizeSlider?.value() || 50;
-    switch (figureName) {
-        case "None":
-            break;
-        case "Cuadrado":
-            return plane(sliderSizeValue);
-            break;
-        case "Cubo":
-            return box(sliderSizeValue, sliderSizeValue, sliderSizeValue);
-            break;
-        case "Cilindro":
-            return cylinder(sliderSizeValue, sliderSizeValue);
-            break;
-        case "Cono":
-            return cone(sliderSizeValue, sliderSizeValue);
-            break;
-        case "Toro":
-            return torus(sliderSizeValue, 20);
-            break;
-        case "Esfera":
-            return sphere(sliderSizeValue);
-            break;
-    }
+  let sliderSizeValue = figureSizeSlider?.value() || 50;
+  switch (figureName) {
+    case "None":
+      break;
+    case "Cuadrado":
+      return plane(sliderSizeValue);
+      break;
+    case "Cubo":
+      return box(sliderSizeValue, sliderSizeValue, sliderSizeValue);
+      break;
+    case "Cilindro":
+      return cylinder(sliderSizeValue, sliderSizeValue);
+      break;
+    case "Cono":
+      return cone(sliderSizeValue, sliderSizeValue);
+      break;
+    case "Toro":
+      return torus(sliderSizeValue, 20);
+      break;
+    case "Esfera":
+      return sphere(sliderSizeValue);
+      break;
+  }
 }
 
-optionsButton = ["colorButton", "backgroundButton"]
+optionsButton = ["colorButton", "backgroundButton"];
 
-buttonState = optionsButton.map(buttonClass => { buttonClass: false });
+buttonState = optionsButton.map((buttonClass) => {
+  buttonClass: false;
+});
 
 /*
 //Agrego los detectores de click a los botoens
@@ -170,76 +164,80 @@ optionsButton.forEach(buttonClass => {
 */
 
 function applyBackgroundColor() {
-    if (buttonState["backgroundButton"]) {
-        const r = backgroundSliders[0]?.value() || 0;
-        const g = backgroundSliders[1]?.value() || 0;
-        const b = backgroundSliders[2]?.value() || 0;
-        background(r, g, b);
-    }
+  if (buttonState["backgroundButton"]) {
+    const r = backgroundSliders[0]?.value() || 0;
+    const g = backgroundSliders[1]?.value() || 0;
+    const b = backgroundSliders[2]?.value() || 0;
+    background(r, g, b);
+  }
 }
 
 function applyBackgroundColorToFigure() {
-    if (buttonState["colorButton"]) {
-        const r = figureColorSliders[0]?.value() || 0;
-        const g = figureColorSliders[1]?.value() || 0;
-        const b = figureColorSliders[2]?.value() || 0;
-        fill(r, g, b);
-    }
+  if (buttonState["colorButton"]) {
+    const r = figureColorSliders[0]?.value() || 0;
+    const g = figureColorSliders[1]?.value() || 0;
+    const b = figureColorSliders[2]?.value() || 0;
+    fill(r, g, b);
+  }
 }
-
 
 // Hay un bug de processing a la hora de colocar los textos con videos
 // Por lo cual creo la funcion yo
 // ATT EL DAMI
 function setTextInProcessing(text, positionX, positionY) {
-    let spanElement = document.createElement("span");
-    //Si vas a cambiar algo, no cambies esto
-    //Uso textNode para no regenerar el arbol
-    //NO CAMBIAR POR INNER HTML o alguna variante
-    let textElement = document.createTextNode(text);
-    //Le agrego el texto al elemento span
-    spanElement.appendChild(textElement);
-    //Te devuelve una referencia al elemento html
-    let spanRef = document.getElementsByTagName("body")[0].appendChild(spanElement)
-    spanRef.style.position = "absolute";
-    spanRef.style.left = positionX + "px";
-    spanRef.style.top = positionY + "px";
-    return spanRef;
+  let spanElement = document.createElement("span");
+  //Si vas a cambiar algo, no cambies esto
+  //Uso textNode para no regenerar el arbol
+  //NO CAMBIAR POR INNER HTML o alguna variante
+  let textElement = document.createTextNode(text);
+  //Le agrego el texto al elemento span
+  spanElement.appendChild(textElement);
+  //Te devuelve una referencia al elemento html
+  let spanRef = document.getElementsByTagName("body")[0].appendChild(spanElement);
+  spanRef.style.position = "absolute";
+  spanRef.style.left = positionX + "px";
+  spanRef.style.top = positionY + "px";
+  return spanRef;
 }
 
 function createSlidersInProcessing(positionX, positionY, sliderAmount, sliderText) {
-    slidersRef = []
-    setTextInProcessing(sliderText, positionX, positionY)
-    for (let sliderIndex = 0; sliderIndex < sliderAmount; sliderIndex++) {
-        let rSlider = createSlider(0, 255, 100);
-        rSlider.position(positionX, positionY + 20 + 30 * sliderIndex);
-        slidersRef.push(rSlider)
-    }
-    return slidersRef;
+  slidersRef = [];
+  setTextInProcessing(sliderText, positionX, positionY);
+  for (let sliderIndex = 0; sliderIndex < sliderAmount; sliderIndex++) {
+    let rSlider = createSlider(0, 255, 100);
+    rSlider.position(positionX, positionY + 20 + 30 * sliderIndex);
+    slidersRef.push(rSlider);
+  }
+  return slidersRef;
 }
 
-function createSliderInProcessing(positionX, positionY, sliderText, fromValue, toValue, realValue) {
-    setTextInProcessing(sliderText, positionX, positionY)
-    let slidersRef = createSlider(fromValue, toValue, realValue);
-    slidersRef.position(positionX, positionY + 20);
-    return slidersRef;
+function createSliderInProcessing(
+  positionX,
+  positionY,
+  sliderText,
+  fromValue,
+  toValue,
+  realValue
+) {
+  setTextInProcessing(sliderText, positionX, positionY);
+  let slidersRef = createSlider(fromValue, toValue, realValue);
+  slidersRef.position(positionX, positionY + 20);
+  return slidersRef;
 }
-
 
 //Obtener cuanto ocupan x cantidad de sliders con texto (en px)
 function getSlidersWithTextHeight(slidersAmount) {
-    return 20 + 30 * slidersAmount;
+  return 20 + 30 * slidersAmount;
 }
-
 
 //UI functions
 
-const loadingModelContainer = document.getElementById('loading-model-message-container');
+const loadingModelContainer = document.getElementById("loading-model-message-container");
 
 function showModelLoader() {
-    loadingModelContainer.style.visibility = 'visible';
+  loadingModelContainer.style.visibility = "visible";
 }
 
 function hideModelLoader() {
-    loadingModelContainer.style.display = 'none';
+  loadingModelContainer.style.display = "none";
 }
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  printWidth: 90,
+  singleQuote: false,
+  semi: true,
+};
